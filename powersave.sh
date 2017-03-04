eeecb868e60e08878abcdfad69c16691cfd4e928
thexerocouk <toby.reynolds1@gmail.com>
2017-03-04 17:14:27 +1300

    Updated powersaving options

--- powersave.sh
+++ powersave.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-echo " [+] Setting Power Management Options"
-echo 'min_power' > '/sys/class/scsi_host/host0/link_power_management_policy'
-echo 'min_power' > '/sys/class/scsi_host/host1/link_power_management_policy'
-echo 'min_power' > '/sys/class/scsi_host/host2/link_power_management_policy'
-echo 'min_power' > '/sys/class/scsi_host/host3/link_power_management_policy'
-echo 'min_power' > '/sys/class/scsi_host/host4/link_power_management_policy'
-echo 'min_power' > '/sys/class/scsi_host/host5/link_power_management_policy'
-echo '1'> '/sys/module/snd_hda_intel/parameters/power_save'
-echo 'auto' > '/sys/bus/usb/devices/3-4/power/control'
-#echo 'auto' > '/sys/bus/usb/devices/1-1.6/power/control'
-#echo 'auto' > '/sys/bus/usb/devices/1-1.4/power/control'
-#echo 'auto' > '/sys/bus/usb/devices/1-1.3/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1c.1/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1c.2/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1c.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:03:00.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.3/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.2/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1d.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1b.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:1a.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:16.3/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:16.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:14.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:00.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:00:02.0/power/control'
-echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs';
-echo 'auto' > '/sys/bus/pci/devices/0000:00:19.0/power/control'
-echo 'auto' > '/sys/bus/pci/devices/0000:02:00.0/power/control'
-#echo 'auto' > '/sys/bus/usb/devices/2-1.4/power/control';
+echo " [+] Setting SCSI power policy"
+for i in /sys/class/scsi_host/*/link_power_management_policy; do echo 'min_power' > $i; done
+
+echo " [+] Setting USB powersaving options"
+for i in /sys/bus/usb/devices/*/power/control; do echo 'auto' > $i; done
+for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 1 > $i; done
+
+echo " [+] Setting PCI powersaving options"
+for i in /sys/bus/pci/devices/*/power/control; do echo 'auto' > $i; done
+
+echo " [+] Setting additional powersaving options"
 echo 2 > /sys/class/backlight/acpi_video0/brightness
 echo 5 > /proc/sys/vm/laptop_mode
-iw dev wlp3s0 set power_save on
+echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
+echo 1500 > /proc/sys/vm/dirty_writeback_centisecs
+echo 1 > /sys/module/snd_hda_intel/parameters/power_save
+
